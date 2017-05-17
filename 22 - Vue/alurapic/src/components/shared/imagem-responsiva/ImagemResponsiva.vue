44f9bc3886611314d2b05470c9020fc6c36e0a4d
Thiago Capelo <thiagokpelo@gmail.com>
2017-05-17 00:49:44 -0300

    Novos cursos e excluindo diretório de node_modules

---
+++ 22 - Vue/alurapic/src/components/shared/imagem-responsiva/ImagemResponsiva.vue
@@ -0,0 +1,17 @@
+<template>
+  <img :src="url" v-bind:alt="titulo">
+</template>
+
+<script>
+export default {
+
+  props: ['url', 'titulo']
+}
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
---
+++ 22 - Vue/alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="painel">
+    <h3 @dblclick="toggleConteudo" class="painel-titulo">{{ titulo }}</h3>
+    <transition name="painel-fade">
+      <div v-show="visivel" class="painel-conteudo">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+
+    props: ['titulo'],
+
+    data() {
+        
+        return {
+          visivel: true
+        }
+    },
+
+    methods: {
+        toggleConteudo() {
+            this.visivel = !this.visivel;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+/* estilo do painel */ 
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+.painel-fade-enter, .painel-fade-leave-active {
+  opacity: 0;
+}
+
+.painel-fade-enter-active, .painel-fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+/*
+* {
+  box-shadow: 5px 5px 5px rgb(150, 150, 150);
+} */
+</style>
